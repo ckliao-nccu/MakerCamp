8db25b986e657cd6b970285a4cbbd0113c9f8aef
ckliao-nccu <ckliao@g.nccu.edu.tw>
2022-06-06 15:05:07 +0800

    LED test

---
+++ 1. LED/LED.ino
@@ -0,0 +1,17 @@
+// 設定針腳編號
+int led = 2;
+
+// 設定函式，只會執行一次
+void setup() {
+  // 將led針腳初始化為輸出模式
+  pinMode(led, OUTPUT);
+}
+
+// 主函式，會不斷重複執行
+void loop() {
+  digitalWrite(led, HIGH);   // 輸出高電位的訊號到led針腳
+  delay(1000);               // 暫停程式執行一秒鐘
+  digitalWrite(led, LOW);    // 輸出低電位的訊號到led針腳
+  delay(1000);               // 暫停程式執行一秒鐘
+}
+
