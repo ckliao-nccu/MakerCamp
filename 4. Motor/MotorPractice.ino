3676521fc8ece71c8241a9eb1a7ae87d117ebba6
ckliao-nccu <ckliao@g.nccu.edu.tw>
2022-06-09 10:09:39 +0800

    Change terminal to bt

---
+++ 4. Motor/MotorPractice.ino
@@ -0,0 +1,29 @@
+// 右邊馬達的針腳
+#define MOTOR_R1 27
+#define MOTOR_R2 26
+
+// 設定函式，只會執行一次
+void setup() {
+  pinMode(MOTOR_R1, OUTPUT);
+  pinMode(MOTOR_R2, OUTPUT);
+}
+
+// 迴圈函式，會不斷重複執行
+void loop() {
+  // 馬達正轉3秒
+  analogWrite(MOTOR_R1, 0);
+  analogWrite(MOTOR_R2, 180);
+  delay(3000);
+  // 馬達停止1秒
+  analogWrite(MOTOR_R1, 0);
+  analogWrite(MOTOR_R2, 0);
+  delay(1000);
+  // 馬達反轉3秒
+  analogWrite(MOTOR_R1, 180);
+  analogWrite(MOTOR_R2, 0);
+  delay(3000);
+  // 馬達停止1秒
+  analogWrite(MOTOR_R1, 0);
+  analogWrite(MOTOR_R2, 0);
+  delay(1000);
+}
